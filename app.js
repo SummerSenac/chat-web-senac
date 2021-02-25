cea322d62fbba7039ee04782bf70dd5f446a54b3
Henning Summer <henningsummer@recife.pe.senac.br>
2021-02-25 11:10:47 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+/* importar as configurações do servidor */
+var app = require('./config/server');
+
+
+/* parametrizar a porta de escuta */
+var server = app.listen(process.env.PORT || 5000, function(){
+	console.log('Servidor online');
+})
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/* criar a conexão por websocket */
+io.on('connection', function(socket){
+	console.log('Usuário conectou');
+
+	socket.on('disconnect', function(){
+		console.log('Usuário desconectou');
+	});
+
+	socket.on('msgParaServidor', function(data){
+
+		/* dialogo */
+		socket.emit(
+			'msgParaCliente', 
+			{apelido: data.apelido, mensagem: data.mensagem}
+		);
+
+		socket.broadcast.emit(
+			'msgParaCliente', 
+			{apelido: data.apelido, mensagem: data.mensagem}
+		);
+
+		/* participantes */
+		if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+			socket.emit(
+				'participantesParaCliente', 
+				{apelido: data.apelido}
+			);
+
+			socket.broadcast.emit(
+				'participantesParaCliente', 
+				{apelido: data.apelido}
+			);
+		}
+	});
+
+});
---
+++ app/routes/chat.js
@@ -0,0 +1,43 @@
+module.exports = function (application) {
+    application.post('/chat', function (req, res) {
+        var dadosForm = req.body;
+
+        req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty();
+        req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
+
+        var erros = req.validationErrors();
+
+        if (erros) {
+            res.render("index", { validacao: erros })
+            return;
+        }
+
+        application.get('io').emit(
+            'msgParaCliente',
+            { apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat' }
+        )
+
+        res.render("chat", { dadosForm: dadosForm });
+    });
+
+    application.get('/chat', function (req, res) {
+        var dadosForm = req.body;
+
+        req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty();
+        req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
+
+        var erros = req.validationErrors();
+
+        if (erros) {
+            res.render("index", { validacao: erros })
+            return;
+        }
+
+        application.get('io').emit(
+            'msgParaCliente',
+            { apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat' }
+        )
+
+        res.render("chat", { dadosForm: dadosForm });
+    });
+}
