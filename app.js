7d55d54c685fd16ea0af01fdd021099f09abbd33
Jonathan Oliveira <jwsolive21@gmail.com>
2021-03-02 09:35:58 -0300

    teste firebase

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+const firebaseConfig = {
+	// ...
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+var firebaseConfig = {
+    apiKey: "AIzaSyDbYUqR8qs8gdAwQQsjmC3y4L8jUIZ6uP4",
+    authDomain: "chat-web-senac.firebaseapp.com",
+    databaseURL: "https://chat-web-senac-default-rtdb.firebaseio.com",
+    projectId: "chat-web-senac",
+    storageBucket: "chat-web-senac.appspot.com",
+    messagingSenderId: "822450398039",
+    appId: "1:822450398039:web:4459519fadccfb76c7d412",
+    measurementId: "G-P52QG5FXCF"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
 /* importar as configurações do servidor */
 var app = require('./config/server');
 
