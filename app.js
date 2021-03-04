20a63b69e46c45fc2ce8b86f73649ead898891db
Ananda Fidelis <anandacfesilva@recife.pe.senac.br>
2021-03-04 08:45:41 -0300

    ananda pwa

---
+++ app.js
@@ -0,0 +1,64 @@
+/* importar as configurações do servidor */
+var app = require('./config/server');
+
+
+/* parametrizar a porta de escuta */
+var server = app.listen(process.env.PORT || 5000, function () {
+	console.log('Servidor online');
+})
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/* criar a conexão por websocket */
+io.on('connection', function (socket) {
+	console.log('Usuário conectou');
+
+	socket.on('disconnect', function () {
+		console.log('Usuário desconectou');
+	});
+
+	socket.on('msgParaServidor', function (data) {
+		console.log('msg serv: ', data)
+		var avaliar = data.mensagem
+		switch (avaliar) {
+			case 'HTML':
+				console.log('bot: ', avaliar);
+				socket.emit(
+					'msgParaCliente',
+					{ apelido: 'Bot Web', mensagem: 'É uma linguagem de estururar documentos' }
+				);
+				break;
+
+			default:
+				/* dialogo */
+				socket.emit(
+					'msgParaCliente',
+					{ apelido: data.apelido, mensagem: data.mensagem, hora: data.hora }
+				);
+		
+				socket.broadcast.emit(
+					'msgParaCliente',
+					{ apelido: data.apelido, mensagem: data.mensagem, hora: data.hora }
+				);
+		
+				/* participantes */
+				if (parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+					socket.emit(
+						'participantesParaCliente',
+						{ apelido: data.apelido }
+					);
+		
+					socket.broadcast.emit(
+						'participantesParaCliente',
+						{ apelido: data.apelido }
+					);
+				}
+
+				break;
+		}
+
+	});
+
+});
---
+++ app/routes/index.js
@@ -0,0 +1,20 @@
+module.exports = function (application) {
+
+    application.get('/', function (req, res) {
+        // application.app.controllers.index.home( application, req, res);
+        // var dadosForm = req.body;
+
+        // req.assert('apelido', 'Nome ou Apelido é obrigatório').notEmpty();
+        // req.assert('apelido', 'Nome ou Apelido deve conter entre 3 e 15 caracteres').len(3, 15);
+
+        // var erros = req.getValidationResult()
+
+        res.render('index', {validacao : {errors : []}});
+        // if (erros) {
+        //     return;
+        // }
+
+        // res.render('chat');
+    });
+
+}
